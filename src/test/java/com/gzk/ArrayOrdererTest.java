a8bc5271df3e0157a39f30eceaf916d3799bb8f8
Igorocky <Igorocky@users.noreply.github.com>
2016-02-19 19:48:10 +0100

    Add sortAscending2

--- src/test/java/com/gzk/ArrayOrdererTest.java
+++ src/test/java/com/gzk/ArrayOrdererTest.java
@@ -5,9 +5,21 @@ import org.junit.Test;
 
 public class ArrayOrdererTest {
     @Test
-    public void test1() {
+    public void test1_1() {
         int[] arr = new int[]{4,5,9,1,7};
         new ArrayOrdererImpl().sortAscending(arr);
+
+        Assert.assertEquals(1, arr[0]);
+        Assert.assertEquals(4, arr[1]);
+        Assert.assertEquals(5, arr[2]);
+        Assert.assertEquals(7, arr[3]);
+        Assert.assertEquals(9, arr[4]);
+    }
+
+    @Test
+    public void test1_2() {
+        int[] arr = new int[]{4,5,9,1,7};
+        new ArrayOrdererImpl().sortAscending2(arr);
 
         Assert.assertEquals(1, arr[0]);
         Assert.assertEquals(4, arr[1]);
@@ -26,5 +38,29 @@ public class ArrayOrdererTest {
         Assert.assertEquals(5, arr[2]);
         Assert.assertEquals(4, arr[3]);
         Assert.assertEquals(1, arr[4]);
+    }
+
+    @Test
+    public void test3_1() {
+        int[] arr = new int[]{4,5,1,1,7};
+        new ArrayOrdererImpl().sortAscending(arr);
+
+        Assert.assertEquals(1, arr[0]);
+        Assert.assertEquals(1, arr[1]);
+        Assert.assertEquals(4, arr[2]);
+        Assert.assertEquals(5, arr[3]);
+        Assert.assertEquals(7, arr[4]);
+    }
+
+    @Test
+    public void test3_2() {
+        int[] arr = new int[]{4,5,1,1,7};
+        new ArrayOrdererImpl().sortAscending2(arr);
+
+        Assert.assertEquals(1, arr[0]);
+        Assert.assertEquals(1, arr[1]);
+        Assert.assertEquals(4, arr[2]);
+        Assert.assertEquals(5, arr[3]);
+        Assert.assertEquals(7, arr[4]);
     }
 }
